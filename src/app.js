6e0be7bfcb0e7c17369c3096724ddd25920e0b05
Archie <archie.wort@nhs.net>
2023-02-20 12:38:40 +0000

    chore(routes) added initial app setup and routing files

--- src/app.js
+++ src/app.js
@@ -1,5 +1,33 @@
-const express = require('express');
+const express = require("express");
+const nunjucks = require("nunjucks");
+const createError = require("http-errors");
 
 const app = express();
+
+// app routers
+const indexRouter = require("./routes/index");
+const authRouter = require("./routes/auth");
+
+// nunjucks configuration
+nunjucks.configure(["./src/views", "./src/_layouts"], {
+  autoescape: true,
+  express: app,
+});
+// view engine setup
+app.set("view engine", "njk");
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// static assets (main.css)
+app.use(express.static(path.join("./", "public")));
+
+// app routes
+app.use("/", indexRouter);
+app.use("/", authRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
 
 module.exports = app;
