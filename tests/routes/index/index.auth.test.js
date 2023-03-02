543f34ac05b63059288ce409f25a56fd6aa53a4c
Archie <archie.wort@nhs.net>
2023-03-02 15:23:04 +0000

    chore(tests) GET index routing tests

--- tests/routes/index/index.auth.test.js
+++ tests/routes/index/index.auth.test.js
@@ -2,14 +2,21 @@ const request = require("supertest");
 const cheerio = require('cheerio');
 const ensureLoggedIn = require("connect-ensure-login").ensureLoggedIn;
 const app = require("../../../src/app");
+const { fetchAssetsForAdmin, fetchAssets, fetchAssetById, updateAssetById} = require("../../../src/middleware/asset");
 
 jest.mock('connect-ensure-login', () => ({
   ensureLoggedIn: jest.fn(() => (req, res, next) => next()),
 }));
 
-afterEach(() => {
-  jest.resetModules();
-  jest.restoreAllMocks();
+jest.mock('../../../src/middleware/asset', () => ({
+  fetchAssetsForAdmin: jest.fn(),
+  fetchAssets: jest.fn(),
+  fetchAssetById: jest.fn(),
+  updateAssetById: jest.fn(),
+}));
+
+beforeEach(() => {
+  jest.clearAllMocks();
 });
 
 describe("GET /add", () => {
@@ -45,4 +52,49 @@ describe("GET /settings", () => {
   });
 });
 
+describe("GET /1/edit", () => {
+  const id = 1;
+  const url = '/' + id + '/edit';
+  fetchAssetById.mockImplementation((req, res, next) => {
+    res.locals.asset = { id: 1, name: "test asset" };
+    next();
+  });
+  it("should return 200 status code match snapshot", async () => {
+    const result = await request(app).get(url).expect(200);
+    expect(result.get('Content-Type')).toContain('text/html');
+    expect(result.text).toMatchSnapshot();
+  });
 
+  it("should render edit content with correct ID", async () => {
+    const result = await request(app).get(url).expect(200);
+
+    const $ = cheerio.load(result.text);
+    const heading = $('h3').text();
+    expect(heading).toBe('Editing asset: #' + id);
+    expect($('a').length).toBe(3);
+  });
+});
+
+describe("GET /1/view", () => {
+  const id = 1;
+  const url = '/' + id + '/view';
+  updateAssetById.mockImplementation((req, res, next) => next());
+  fetchAssetById.mockImplementation((req, res, next) => {
+    res.locals.asset = { id: 1, name: "test asset" };
+    next();
+  });
+  it("should return 200 status code match snapshot", async () => {
+    const result = await request(app).get(url).expect(200);
+    expect(result.get('Content-Type')).toContain('text/html');
+    expect(result.text).toMatchSnapshot();
+  });
+
+  it("should render view content with correct ID", async () => {
+    const result = await request(app).get(url).expect(200);
+
+    const $ = cheerio.load(result.text);
+    const heading = $('h3').text();
+    expect(heading).toBe('Viewing asset: #' + id);
+    expect($('a').length).toBe(3);
+  });
+});
